2af60a29f05fb84838dde46d3d635660e799aa60
tangye <tangye1234@gmail.com>
2023-07-28 13:30:29 +0800

    initial

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function TestPage() {
+  return <span>test</span>
+}
